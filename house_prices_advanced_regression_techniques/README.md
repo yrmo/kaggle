64a2d4ddb734f5a19fd98b9176dcb65d5a877131
Ryan Moore <ryanm.inbox@gmail.com>
2023-11-12 23:33:03 -0500

    house-prices 7.03124: loss.item()=5868421632.0 LotArea, loss plot

---
+++ house_prices_advanced_regression_techniques/README.md
@@ -0,0 +1,3 @@
+# house-prices-advanced-regression-techniques
+
+![](LossVersusEpoch.png)
--- house_prices_advanced_regression_techniques/house.py
+++ house_prices_advanced_regression_techniques/house.py
@@ -1,9 +1,11 @@
 from os import environ
 from typing import Final
 
+import matplotlib.pyplot as plt  # type: ignore
 import pandas as pd
 import torch
 import torch.nn as nn
+import torch.optim as optim
 
 torch.manual_seed(90053)
 
@@ -16,17 +18,59 @@ test_data = pd.read_csv(
     "/kaggle/input/house-prices-advanced-regression-techniques/test.csv"
 )
 
+INPUTS = ["LotArea"]
 
-class ZeroNet(nn.Module):
+y_train = torch.tensor(train_data[["SalePrice"]].to_numpy(), dtype=torch.float32)
+X_train = torch.tensor(train_data[INPUTS].to_numpy(), dtype=torch.float32)
+X_train = (X_train - X_train.mean()) / X_train.std()  # fixes exploding gradients!
+
+print(f"{y_train.size()=}", f"{X_train.size()=}")
+assert X_train is not None
+assert y_train is not None
+assert type(X_train) == torch.Tensor
+assert type(y_train) == torch.Tensor
+
+
+class Neuron(nn.Module):
     def __init__(self):
         super().__init__()
+        self.fc1 = nn.Linear(len(INPUTS), 1)
 
     def forward(self, x):
-        return torch.zeros(x.size(0), 1)
+        x = self.fc1(x)
+        return x
 
 
-model = ZeroNet()
+model = Neuron()
+criterion = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+epochs, losses = [], []
+
+EPOCHS = 3000
+for epoch in range(EPOCHS):
+    optimizer.zero_grad()
+    output = model(X_train)
+    loss = criterion(output, y_train)
+    loss.backward()
+    optimizer.step()
+    if epoch % (EPOCHS // 10) == 0:
+        epochs.append(epoch)
+        losses.append(loss.item())
+        print(f"{loss.item()=}")
+
 submission = pd.DataFrame()
-submission["Id"] = test_data["Id"]
-submission["SalePrice"] = model(torch.tensor(test_data.Id))
+submission["Id"] = test_data.Id
+with torch.no_grad():
+    submission["SalePrice"] = model(
+        torch.tensor(test_data[INPUTS].to_numpy(), dtype=torch.float32)
+    )
 submission.to_csv("/kaggle/working/submission.csv", index=False)
+
+if not SUBMIT:
+    plt.plot(epochs, losses, label="Training Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.savefig("./house_prices_advanced_regression_techniques/LossVersusEpoch.png")
+    plt.show()
